2b474026844ddb666968955abe54fe9d377f4648
Jelly <Jellybob2016@outlook.com>
2023-12-05 04:52:21 +0000

    Adjust template

--- template.py
+++ template.py
@@ -12,6 +12,9 @@ def main(file_input):
 
 file = aoc_input()
 aoc_answer = ic(main(file))
+
 correct_answer = get_answer()
+ic(correct_answer)
+
 is_correct = correct_answer == aoc_answer if correct_answer else "Unknown"
 ic(is_correct)
